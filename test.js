8da446018e66a926a5db64efe247fd2699ec1f71
Peter Moresi <peter.moresi@gmail.com>
2015-10-22 20:46:29 -0700

    Fix test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var find = require('./FIND');
 
-test('it should upper case a string', (t) => {
+test('it should upper case a string', function(t) {
   t.plan(3);
   t.equal( find( 'a', 'fooabar'), 4)
   t.equal( find( 'bar', 'fooabar'), 5)
